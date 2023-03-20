682a9d72f04029f2dbce0f6919da3a13b7d422b6
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2023-03-20 12:56:06 +0100

    service

---
+++ src/main/java/com/example/smartcity/service/AuthService.java
@@ -0,0 +1,18 @@
+package com.example.smartcity.service;
+
+import com.example.smartcity.handler.Handler;
+import com.example.smartcity.model.AccessoLogin;
+
+public class AuthService {
+    private final Handler handler;
+
+    public AuthService(Handler handler){this.handler = handler;}
+    public AccessoLogin logIn(String email, String password){
+        AccessoLogin result = handler.handle(email,password);
+        if (result == AccessoLogin.SUCCESSO){
+            System.out.println("Autorizzazione effettuata con successo!!!");
+            return result;
+        }
+        return result;
+    }
+}
