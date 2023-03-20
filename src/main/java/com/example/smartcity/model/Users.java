98c9d7f25ef24becd20e6e39de968603a1774bae
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2023-03-20 13:03:43 +0100

    public interface

---
+++ src/main/java/com/example/smartcity/model/Users.java
@@ -0,0 +1,10 @@
+package com.example.smartcity.model;
+
+public interface Users {
+    String getUsername();
+    void setUsername(String username);
+
+    String getPassword();
+    void setPassword(String password);
+
+}
