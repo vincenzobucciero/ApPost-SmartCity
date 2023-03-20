88454564de84bd512185c3651d97caf3627f2874
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2023-03-20 13:02:56 +0100

    never used

---
+++ src/main/java/com/example/smartcity/model/LoginDao.java
@@ -0,0 +1,12 @@
+package com.example.smartcity.model;
+
+import com.example.smartcity.handler.PasswordHandler;
+
+
+public class LoginDao {
+    public DB users;
+
+    public LoginDao() {
+        users = DB.getIstanza();
+    }
+}
