f5cee67bd96665ecfd765cc33f9f118a6e1a9305
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2023-03-20 12:59:58 +0100

    abstract handler

---
+++ src/main/java/com/example/smartcity/handler/Handler.java
@@ -0,0 +1,33 @@
+package com.example.smartcity.handler;
+
+import com.example.smartcity.model.AccessoLogin;
+
+public abstract class Handler {
+    private Handler next;
+    public Handler setNextHandler(Handler next){
+        this.next = next;
+        return  next;
+    }
+
+    public Handler getNext() {
+        return next;
+    }
+
+    public abstract AccessoLogin handle(String username, String password);
+
+    protected AccessoLogin handlerNext(String username, String password){
+        if(this.next == null){
+            return AccessoLogin.SUCCESSO;
+        }
+        return next.handle(username, password);
+    }
+
+    //public abstract boolean handle(String username, String password);
+
+    /*protected boolean handleNext(String username, String password){
+        if(next == null){
+            return true;
+        }
+        return next.handle(username, password);
+    }*/
+}
