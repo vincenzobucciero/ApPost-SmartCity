1598360fcd7e155e0f2511802b7d28cf9a19eda6
Vincenzo Bucciero <74311987+vincenzobucciero@users.noreply.github.com>
2023-05-03 15:08:38 +0200

    javadoc ok

--- src/main/java/com/example/smartcity/service/CommandPrezzo/Invoker.java
+++ src/main/java/com/example/smartcity/service/CommandPrezzo/Invoker.java
@@ -5,7 +5,7 @@ import com.example.smartcity.model.Bean.ParkingBean;
 
 
 /**
- * La classe Invoker è un'implementazione del pattern Command e si occupa d'invocare un comando specifico
+ * La classe Invoker è una classe adibita all'invocazione un comando specifico 
  * associato a un'operazione selezionata dall'utente tramite l'enum VeicoliEnum.
  * Il metodo getTot restituisce il totale del prezzo per l'operazione richiesta, mentre il metodo getPosti
  * restituisce il numero di posti disponibili per l'operazione richiesta.
